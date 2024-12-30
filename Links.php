6120b73bcdd349c6db65f404ef985ab3026af5b1
Eamonn Duffy <Eamonn@Duffy.global>
2024-12-30 19:31:27 +0000

    feat: Reorganised the order of Links post ESTA Application.

--- Links.php
+++ Links.php
@@ -60,13 +60,13 @@
 ?>
 		<div id="Middle">
 		<p>
-			&nbsp;&nbsp;0. <a target="_blank" href="https://www.Rapture.global/">Rapture Web Site</a>
+			&nbsp;&nbsp;0. <a target="_blank" href="https://www.Eadent.com/Videos/The National D-Day Memorial - 3 Overlord Cir, Bedford, VA 24523, United States - 20150417_133020.mp4">The National D-Day Memorial, Virginia 24523, United States (Video)</a>
 		</p>
 		<p>
-			&nbsp;&nbsp;1. <a target="_blank" href="https://www.RaptureTherapy.global/">Rapture Therapy Web Site<a>
+			&nbsp;&nbsp;1. <a target="_blank" href="https://www.BritishLegion.org.uk/">The Royal British Legion</a>
 		</p>
 		<p>
-			&nbsp;&nbsp;2. <a target="_blank" href="https://www.Duffy.global/">Duffy Web Site</a>
+			&nbsp;&nbsp;2. <a target="_blank" href="https://www.Canva.com/">Canva Design</a>
 		</p>
 		<p>
 			&nbsp;&nbsp;3. <a target="_blank" href="https://www.Ghis.eu/">HSE EVE GHIS</a>
@@ -78,28 +78,28 @@
 			&nbsp;&nbsp;5. <a target="_blank" href="https://Web.Ghis.eu/Portfolio_EAD.html">My Web Design Portfolio at HSE EVE GHIS</a>
 		</p>
 		<p>
-			&nbsp;&nbsp;6. <a target="_blank" href="https://www.Canva.com/">Canva Design</a>
+			&nbsp;&nbsp;6. <a target="_blank" href="https://www.EamonnDuffy.com/">Eamonn Duffy's Web Site</a>
 		</p>
 		<p>
-			&nbsp;&nbsp;7. <a target="_blank" href="https://www.GitHub.com/Eadent/">Eadent GitHub Repositories</a>
+			&nbsp;&nbsp;7. <a target="_blank" href="https://EamonnAnthonyDuffy.WordPress.com/">Eamonn Duffy's Personal Blog</a>
 		</p>
 		<p>
-			&nbsp;&nbsp;8. <a target="_blank" href="https://www.GitHub.com/Rapture-Global/">Rapture Global GitHub Repositories</a>
+			&nbsp;&nbsp;8. <a target="_blank" href="https://www.Duffy.global/">Duffy Web Site</a>
 		</p>
 		<p>
-			&nbsp;&nbsp;9. <a target="_blank" href="https://www.Eadent.com/Videos/The National D-Day Memorial - 3 Overlord Cir, Bedford, VA 24523, United States - 20150417_133020.mp4">The National D-Day Memorial, Virginia 24523, United States (Video)</a>
+			&nbsp;&nbsp;9. <a target="_blank" href="https://www.GitHub.com/Eadent/">Eadent GitHub Repositories</a>
 		</p>
 		<p>
-			10. <a target="_blank" href="https://www.BritishLegion.org.uk/">The Royal British Legion</a>
+			10. <a target="_blank" href="https://www.GitHub.com/Rapture-Global/">Rapture Global GitHub Repositories</a>
 		</p>
 		<p>
-			11. <a target="_blank" href="https://www.LestWeForget.one/">Lest We Forget Web Site</a>
+			11. <a target="_blank" href="https://www.RaptureTherapy.global/">Rapture Therapy Web Site<a>
 		</p>
 		<p>
-			12. <a target="_blank" href="https://www.EamonnDuffy.com/">Eamonn Duffy's Web Site</a>
+			12. <a target="_blank" href="https://www.Rapture.global/">Rapture Web Site</a>
 		</p>
 		<p>
-			13. <a target="_blank" href="https://EamonnAnthonyDuffy.WordPress.com/">Eamonn Duffy's Personal Blog</a>
+			13. <a target="_blank" href="https://www.LestWeForget.one/">Lest We Forget Web Site</a>
 		</p>
 		<p style="margin: 0; padding: 0;">
 		<br />
